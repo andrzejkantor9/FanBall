874e02f32c797ce94f24894bad5332f007a726e1
DESKTOP-HN11TL7\ander <andrzejkantor9@gmail.com>
2021-01-06 13:42:10 +0100

    enable fan by input, disable extra force by distance to fan

--- Source/FanBall/FanBallGameMode.cpp
+++ Source/FanBall/FanBallGameMode.cpp
@@ -8,7 +8,7 @@ AFanBallGameMode::AFanBallGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_ThirdPersonCharacter"));
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_FanBallCharacterMain"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Obsolete/BP_FanBallCharacterMain"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
