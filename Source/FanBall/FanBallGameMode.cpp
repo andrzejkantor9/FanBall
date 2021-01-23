7737e3e5da3cf3e8a60e25549cdca6c2c8bf922c
DESKTOP-HN11TL7\ander <andrzejkantor9@gmail.com>
2021-01-23 12:19:07 +0100

    widgets, win condition, move default pawn,

--- Source/FanBall/FanBallGameMode.cpp
+++ Source/FanBall/FanBallGameMode.cpp
@@ -8,9 +8,9 @@ AFanBallGameMode::AFanBallGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_ThirdPersonCharacter"));
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Obsolete/BP_FanBallCharacterMain"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Obsolete/BP_FanBallCharacterMain"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+	//	DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
 }
